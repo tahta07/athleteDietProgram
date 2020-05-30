9869420f47b0af05d12851cd133ea2de1e037aa2
Yunus Erol <erol_yunus@hotmail.com>
2020-05-30 10:38:00 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,105 @@
+class User {
+    constructor(name, age, mail) {
+        this.name = name
+        this.age = age
+        this.mail = mail
+        this.followers = []
+        this.follows = []
+        this.recipe = []
+        this.dailyPrograms = []
+    }
+    followUser(user) {
+        user.followers.push(this)
+        this.follows.push(user)
+        console.log(`${this.name}: ${user.name} adlı kullanıcıyı takip ediliyor. listesinde takip ettiği ${this.follows.length} kişi bulunmaktadır.`)
+    }
+    postRecipie(recipe) {
+        this.recipe.push(recipe)
+    }
+    createDailyProgram(dailyPrograms) {
+        this.dailyPrograms.push(dailyPrograms)
+    }
+}
+
+class Ingredients {
+    constructor(name, calori, protein, carb, fat) {
+        this.name = name
+        this.calori = calori
+        this.protein = protein
+        this.carb = carb
+        this.fat = fat
+    }
+}
+
+
+class Recipe {
+    constructor(name) {
+        this.name = name
+        this.totalCalories = 0
+        this.ingredients = []
+    }
+    addIngredients(recipeIngredients) {
+        this.totalCalories = recipeIngredients.calori + this.totalCalories
+        this.ingredients.push(recipeIngredients.name)
+    }
+}
+
+class Meal {
+    constructor(name) {
+        this.name = name
+        this.ingredients = []
+        this.calori = 0
+    }
+    addIngredients(ingredients) {
+        this.ingredients.push(ingredients.name)
+        this.calori = this.calori + ingredients.calori
+    }
+    addRecipe(recipe) {
+        this.ingredients.push(recipe.ingredients)
+        this.calori = this.calori + recipe.totalCalories
+    }
+}
+
+class DailyPrograms {
+    constructor(name) {
+        this.name = name
+        this.meal = []
+        this.dailyTotalCalori = 0
+    }
+    addMeal(name) {
+        this.meal.push(name)
+        this.dailyTotalCalori = this.dailyTotalCalori + name.calori
+    }
+}
+
+const yumurta = new Ingredients('Yumurta', 100, 10, 5, 3)
+const sut = new Ingredients('Süt', 125, 2, 12, 20)
+const sabah = new Meal('Sabah')
+const sutluYumurta = new Recipe('Sütlü Yumurta')
+const diyet1 = new DailyPrograms('1.Hafta')
+
+
+sutluYumurta.addIngredients(sut)
+sutluYumurta.addIngredients(yumurta)
+sabah.addIngredients(yumurta)
+sabah.addIngredients(sut)
+// sabah.addRecipe(sutluYumurta)
+diyet1.addMeal(sabah)
+
+const yunus = new User('Yunus', 30, 'yunus@yunus.com')
+const naciye = new User('Naciye', 25, 'naciye@naciye.com')
+const armagan = new User('Armağan', 30, 'armagan@armagan.com')
+
+armagan.followUser(yunus)
+naciye.followUser(yunus)
+yunus.followUser(naciye)
+yunus.followUser(armagan)
+yunus.createDailyProgram(diyet1)
+naciye.createDailyProgram(diyet1)
+
+// console.log(sabah)
+// console.log(sutluYumurta)
+// console.log(yumurta)
+// yunus.postRecipie(sutluYumurta)
+// console.log(sabah)
+console.log(yunus)
---
+++ plantUml/readme.md
@@ -0,0 +1,48 @@
+Sporcu Beslenme Programı
+Kullanıcı Kayıt olur bu sırada "e-mail","isim" ve "lokasyon" girer (lokasyon ileride çevredeki marketlere göre yemek seçimi yapmak için kullanılabilir)
+
+Kayıt olduktan sonra "ÖLÇÜM" Bölümüne yönlendirilir.
+Ölçümde
+-------
+-Boy
+-Kilo
+-Yaş
+-Cinsiyet
+-Günlük yaptığı egzersiz ( Hareketsiz, Az hareketli, Hareketli, Aktif)
+-Yağ Oranı (Eğer yağ oranı bilinmiyorsa Boyun, Göğüs, Bel ve Kalça ölçümü istenecek)
+
+Ölçüm sonucunda 
+-Yağ Oranı
+-BMI
+-Günlük Alması Gereken Kalori
+-Makrolar (Karbonhidrat, Yağ, Protein)
+
+Ölçüm sonucuna göre kullanıcıdan bir hedef istenir bu Hedef Kilo almak Yağ Yakmak ve Kiloyu Korumak olarak belirlenecek ve Günlük alması gereken kalori miktarından buna göre -500, 0, +500 ekleme yapılarak makrolar tekrar hesaplanacak
+
+----------------------------------------------------------
+İkinci kısımda Kullanıcıya bir dashbord verilecek burada Günlük Haftalık programları olacak.
+Günlük yediklerini kaydedeceği buna göre alması gereken kalori ve makro miktarları hesaplanacak.
+Bu kısımda Databaseden yiyecekleri seçerek ekleyecek eğer yoksa kendisi database e ekleyebilecek.
+
+Sürekli yediği yiyeceklerden tarifler oluşturabilecek bunları tek seferde ekleyebilecek. (Örneğin tavuk sote = tek tek tüm malzemeleri eklemek yerine bir tarif kaydedip daha sonra tavuk soteyi ekleyip tek tek eklemekten kurtulacak)
+
+Günlük programları kaydedip haftalık program oluşturabilecek.
+Kullanıcı günlük programını oluşturabileceği gibi sistemden otomatik tarifler de talep edebilecek.
+Bu otomatik program kısmı 3 bölümden oluşacak.
+-Günlük Cost(ücret) Belirlenip ona göre yiyecek seçimi ( Lokasyon bilgisiyle koordine çalışıp çevre marketlere göre seçebilir)
+-Random
+-Kullanıcının seçeceği elinde olan yiyeceklere göre
+
+Bu programı hesaplarken kullanıcının alması gereken makro ve kalori ihtiyacına göre oluşturacak.
+--------------------------------------------------------------------
+Üçüncü kısım 
+Kullanıcı kendi yemek tariflerini de oluşturup paylaşabilecek diğer kullanıcılar bu tarifi kendi programlarına ekleyebilecek beğebip oylayabilecek yorum yapabilecek.
+Kullanıcılar diğer kullanıcıları takip edip paylaştığı tarifleri görebilecek.
+
+
+
+
+
+
+
+
